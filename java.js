5c8edb05adc430e81d2608737115e6394705e825
VirsatusCodes <donavynpence@yahoo.com>
2022-04-27 14:15:24 -0700

    functional rps using buttons. incorporated javascript eventlisteners to play rounds of rps and display the results of them, as well as the overall result of the match. also fixed an error and betterd my undersatnding of setting up containers for my text content or other content.

--- java.js
+++ java.js
@@ -1,10 +1,9 @@
   // using strict to build good habits
   "use strict";
-  let num = 14
   let playerSelection = "rock";
   let computerSelection = "tart";
-  let score = 0
-  
+  let playerScore= 0
+  let computerScore= 0
   //creates a random number and assigns the computers choice based on it
   function computerPlay(num) {
     num = Math.random();
@@ -13,32 +12,46 @@
     else return "scissors";
   }
   // gives playerSelection and computerSelection a value and assigns an outcome based on that
-  function playRound(playerSelection, computerSelection) {
-   playerSelection= prompt("rock, paper, or scissors?").toLowerCase() ;
+  function playRound(playerSelection) {
    computerSelection =computerPlay();
-      if (playerSelection === computerSelection) return "tie! wow that was a close one... not.";
-      else if (playerSelection==="paper" && computerSelection==="rock") return "You Win! Paper beats Rock";
-      else if (playerSelection==="rock" && computerSelection==="scissors") return "You Win! Rock beats Scissors";
-      else if (playerSelection==="scissors" && computerSelection==="paper") return "You Win! Scissors beats Paper";
-      else if (playerSelection==="rock" && computerSelection==="paper") return "You Lose! Rock loses to Paper";
-      else if (playerSelection==="scissors" && computerSelection==="rock") return "You Lose! Scissors lose to Rock";
-      else if (playerSelection==="paper" && computerSelection==="scissors") return "You Lose! Paper loses to Scissors";
-      else return"Do you think this is some sort of joke??? Instant loss.";
+   console.log(playerSelection)
+      if (playerSelection === computerSelection) round.textContent = "tie! wow that was a close one... not.";
+      else if (playerSelection==="paper" && computerSelection==="rock") round.textContent = "You Win! Paper beats Rock";
+      else if (playerSelection==="rock" && computerSelection==="scissors") round.textContent = "You Win! Rock beats Scissors";
+      else if (playerSelection==="scissors" && computerSelection==="paper") round.textContent = "You Win! Scissors beats Paper";
+      else if (playerSelection==="rock" && computerSelection==="paper") round.textContent = "You Lose! Rock loses to Paper";
+      else if (playerSelection==="scissors" && computerSelection==="rock") round.textContent = "You Lose! Scissors lose to Rock";
+      else if (playerSelection==="paper" && computerSelection==="scissors") round.textContent = "You Lose! Paper loses to Scissors";
+      if (round.textContent.includes("Win")) playerScore += + 1;
+      else if (round.textContent.includes("Lose")) computerScore += + 1; 
+      outcome.textContent = "Player score is :" + ` ${playerScore}` + " Computer score is :" + ` ${computerScore}`;
+      if (playerScore === 5) alert("Harray! you won!");
+      else if (computerScore === 5) alert("Oh no, nonono, NOT LIKE THIS! the computer has won!!!");
   }
-  
-  //console.log it acts the same as a call and lets me see the answer.
-  // makes the game play 5 times and then give a response based on if youve won or lost overall.
-  function game() {
-    for (let i = 0; i < 5; i++) {
-      if (playRound().includes("Win"))
-      score +=  + 1;
-      else score += - 1;
-      console.log(score);
-    }
-    if ( score > 0)
-    alert("youve won this time...");
-    else if (score < 0)
-    alert("BWAHHAHA DEATH IT IS");
-  }
-  //game()
-  //playRound(playerSelection, computerSelection)
+
+  const rock = document.querySelector('#rock');
+  rock.addEventListener('click', () => {
+      playRound("rock")
+  });
+
+  const paper = document.querySelector('#paper');
+  paper.addEventListener('click', () => {
+      playRound("paper")
+  });
+
+  const scissors = document.querySelector('#scissors');
+  scissors.addEventListener('click', () => {
+      playRound("scissors")
+  });
+
+const roundContainer = document.querySelector('#roundContainer');
+const round = document.createElement("p");
+round.classList.add("round");
+round.textContent = "No game has been played yet!";
+roundContainer.appendChild(round);
+
+const outcomeContainer = document.querySelector('#outcomeContainer');
+const outcome = document.createElement("p");
+outcome.classList.add("outcome");
+outcome.textContent = "Player score is :" + ` ${playerScore}` + " Computer score is :" + ` ${computerScore}`;
+outcomeContainer.appendChild(outcome);
